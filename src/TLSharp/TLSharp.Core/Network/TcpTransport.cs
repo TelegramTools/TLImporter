c86f1fae41783a5c9685e7f56cc1234cd090a0e1
ferferga <ferferga@users.noreply.github.com>
2018-03-08 23:42:12 +0100

    Added sources of TLSharp.Core used in TLImporter

---
+++ src/TLSharp/TLSharp.Core/Network/TcpTransport.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TLSharp.Core.Network
+{
+    public delegate TcpClient TcpClientConnectionHandler(string address, int port);
+
+    public class TcpTransport : IDisposable
+    {
+        private readonly TcpClient _tcpClient;
+        private int sendCounter = 0;
+
+        public TcpTransport(string address, int port, TcpClientConnectionHandler handler = null)
+        {
+            if (handler == null)
+            {
+                _tcpClient = new TcpClient();
+
+                var ipAddress = IPAddress.Parse(address);
+                _tcpClient.Connect(ipAddress, port);
+            }
+            else
+                _tcpClient = handler(address, port);
+        }
+
+        public async Task Send(byte[] packet)
+        {
+            if (!_tcpClient.Connected)
+                throw new InvalidOperationException("Client not connected to server.");
+
+            var tcpMessage = new TcpMessage(sendCounter, packet);
+
+            await _tcpClient.GetStream().WriteAsync(tcpMessage.Encode(), 0, tcpMessage.Encode().Length);
+            sendCounter++;
+        }
+
+        public async Task<TcpMessage> Receieve()
+        {
+            var stream = _tcpClient.GetStream();
+
+            var packetLengthBytes = new byte[4];
+            if (await stream.ReadAsync(packetLengthBytes, 0, 4) != 4)
+                throw new InvalidOperationException("Couldn't read the packet length");
+            int packetLength = BitConverter.ToInt32(packetLengthBytes, 0);
+
+            var seqBytes = new byte[4];
+            if (await stream.ReadAsync(seqBytes, 0, 4) != 4)
+                throw new InvalidOperationException("Couldn't read the sequence");
+            int seq = BitConverter.ToInt32(seqBytes, 0);
+
+            int readBytes = 0;
+            var body = new byte[packetLength - 12];
+            int neededToRead = packetLength - 12;
+
+            do
+            {
+                var bodyByte = new byte[packetLength - 12];
+                var availableBytes = await stream.ReadAsync(bodyByte, 0, neededToRead);
+                neededToRead -= availableBytes;
+                Buffer.BlockCopy(bodyByte, 0, body, readBytes, availableBytes);
+                readBytes += availableBytes;
+            }
+            while (readBytes != packetLength - 12);
+
+            var crcBytes = new byte[4];
+            if (await stream.ReadAsync(crcBytes, 0, 4) != 4)
+                throw new InvalidOperationException("Couldn't read the crc");
+            int checksum = BitConverter.ToInt32(crcBytes, 0);
+
+            byte[] rv = new byte[packetLengthBytes.Length + seqBytes.Length + body.Length];
+
+            Buffer.BlockCopy(packetLengthBytes, 0, rv, 0, packetLengthBytes.Length);
+            Buffer.BlockCopy(seqBytes, 0, rv, packetLengthBytes.Length, seqBytes.Length);
+            Buffer.BlockCopy(body, 0, rv, packetLengthBytes.Length + seqBytes.Length, body.Length);
+            var crc32 = new Ionic.Crc.CRC32();
+            crc32.SlurpBlock(rv, 0, rv.Length);
+            var validChecksum = crc32.Crc32Result;
+
+            if (checksum != validChecksum)
+            {
+                throw new InvalidOperationException("invalid checksum! skip");
+            }
+
+            return new TcpMessage(seq, body);
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return this._tcpClient.Connected;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            if (_tcpClient.Connected)
+                _tcpClient.Close();
+        }
+    }
+}
---
+++ src/TLSharp/TLSharp.Core/Session.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.IO;
+using TeleSharp.TL;
+using TLSharp.Core.MTProto;
+using TLSharp.Core.MTProto.Crypto;
+
+namespace TLSharp.Core
+{
+    public interface ISessionStore
+    {
+        void Save(Session session);
+        Session Load(string sessionUserId);
+    }
+
+    public class FileSessionStore : ISessionStore
+    {
+        public void Save(Session session)
+        {
+            using (var stream = new FileStream($"{session.SessionUserId}.dat", FileMode.OpenOrCreate))
+            {
+                var result = session.ToBytes();
+                stream.Write(result, 0, result.Length);
+            }
+        }
+
+        public Session Load(string sessionUserId)
+        {
+            var sessionFileName = $"{sessionUserId}.dat";
+            if (!File.Exists(sessionFileName))
+                return null;
+
+            using (var stream = new FileStream(sessionFileName, FileMode.Open))
+            {
+                var buffer = new byte[2048];
+                stream.Read(buffer, 0, 2048);
+
+                return Session.FromBytes(buffer, this, sessionUserId);
+            }
+        }
+    }
+
+    public class FakeSessionStore : ISessionStore
+    {
+        public void Save(Session session)
+        {
+
+        }
+
+        public Session Load(string sessionUserId)
+        {
+            return null;
+        }
+    }
+
+    public class Session
+    {
+	    private const string defaultConnectionAddress = "149.154.167.50";//Test settings: "149.154.167.40";
+
+        private const int defaultConnectionPort = 443;
+
+        public string SessionUserId { get; set; }
+        public string ServerAddress { get; set; }
+        public int Port { get; set; }
+        public AuthKey AuthKey { get; set; }
+        public ulong Id { get; set; }
+        public int Sequence { get; set; }
+        public ulong Salt { get; set; }
+        public int TimeOffset { get; set; }
+        public long LastMessageId { get; set; }
+        public int SessionExpires { get; set; }
+        public TLUser TLUser { get; set; }
+        private Random random;
+
+        private ISessionStore _store;
+
+        public Session(ISessionStore store)
+        {
+            random = new Random();
+            _store = store;
+        }
+
+        public byte[] ToBytes()
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new BinaryWriter(stream))
+            {
+                writer.Write(Id);
+                writer.Write(Sequence);
+                writer.Write(Salt);
+                writer.Write(LastMessageId);
+                writer.Write(TimeOffset);
+                Serializers.String.write(writer, ServerAddress);
+                writer.Write(Port);
+
+                if (TLUser != null)
+                {
+                    writer.Write(1);
+                    writer.Write(SessionExpires);
+                    ObjectUtils.SerializeObject(TLUser, writer);
+                }
+                else
+                {
+                    writer.Write(0);
+                }
+
+                Serializers.Bytes.write(writer, AuthKey.Data);
+
+                return stream.ToArray();
+            }
+        }
+
+        public static Session FromBytes(byte[] buffer, ISessionStore store, string sessionUserId)
+        {
+            using (var stream = new MemoryStream(buffer))
+            using (var reader = new BinaryReader(stream))
+            {
+                var id = reader.ReadUInt64();
+                var sequence = reader.ReadInt32();
+                var salt = reader.ReadUInt64();
+                var lastMessageId = reader.ReadInt64();
+                var timeOffset = reader.ReadInt32();
+                var serverAddress = Serializers.String.read(reader);
+                var port = reader.ReadInt32();
+
+                var isAuthExsist = reader.ReadInt32() == 1;
+                int sessionExpires = 0;
+                TLUser TLUser = null;
+                if (isAuthExsist)
+                {
+                    sessionExpires = reader.ReadInt32();
+                    TLUser = (TLUser)ObjectUtils.DeserializeObject(reader);
+                }
+
+                var authData = Serializers.Bytes.read(reader);
+
+                return new Session(store)
+                {
+                    AuthKey = new AuthKey(authData),
+                    Id = id,
+                    Salt = salt,
+                    Sequence = sequence,
+                    LastMessageId = lastMessageId,
+                    TimeOffset = timeOffset,
+                    SessionExpires = sessionExpires,
+                    TLUser = TLUser,
+                    SessionUserId = sessionUserId,
+                    ServerAddress = serverAddress,
+                    Port = port
+                };
+            }
+        }
+
+        public void Save()
+        {
+            _store.Save(this);
+        }
+
+        public static Session TryLoadOrCreateNew(ISessionStore store, string sessionUserId)
+        {
+            return store.Load(sessionUserId) ?? new Session(store)
+            {
+                Id = GenerateRandomUlong(),
+                SessionUserId = sessionUserId,
+                ServerAddress = defaultConnectionAddress,
+                Port = defaultConnectionPort
+            };
+        }
+
+        private static ulong GenerateRandomUlong()
+        {
+            var random = new Random();
+            ulong rand = (((ulong)random.Next()) << 32) | ((ulong)random.Next());
+            return rand;
+        }
+
+        public long GetNewMessageId()
+        {
+            long time = Convert.ToInt64((DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds);
+            long newMessageId = ((time / 1000 + TimeOffset) << 32) |
+                                ((time % 1000) << 22) |
+                                (random.Next(524288) << 2); // 2^19
+                                                            // [ unix timestamp : 32 bit] [ milliseconds : 10 bit ] [ buffer space : 1 bit ] [ random : 19 bit ] [ msg_id type : 2 bit ] = [ msg_id : 64 bit ]
+
+            if (LastMessageId >= newMessageId)
+            {
+                newMessageId = LastMessageId + 4;
+            }
+
+            LastMessageId = newMessageId;
+            return newMessageId;
+        }
+    }
+}
